91b4b8dd4ccc47f750bdcef033df1f29bb9411c4
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 17:44:07 +0900

    1-3. Boolean 자료형

--- Chapter1. 자료형과 변수.py
+++ Chapter1. 자료형과 변수.py
@@ -16,3 +16,12 @@ print("Hello World")
 # 문자열 곱셈 가능(이외의 사직연산은 불가)
 print("Hello World"*3)
 
+''' 1-3. Boolean 자료형 '''
+# True / False 을 출력하는 문자열
+print(0<1)
+print(0>1)
+print(0<=1) # 등호(=)는 부등호 오른쪽에 위치해야함(print(0=<1)는 오류)
+print(True)
+print(False) # 시작 문자가 대문자여야 Boolean 으로 인식
+print(not True) # not 은 반대로 출력
+
