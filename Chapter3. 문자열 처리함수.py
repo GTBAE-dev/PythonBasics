91ce47407d53fe8be5f8866f7e4a2ecb04ba38e6
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 18:51:56 +0900

    3-1. 문자열 슬라이싱

---
+++ Chapter3. 문자열 처리함수.py
@@ -0,0 +1,8 @@
+''' 3-1. 문자열 슬라이싱 '''
+text_slicing = "123456789a"
+print(text_slicing[5]) # 변수[a] : 문자열 중 a번째 문자 출력(시작위치 : 0)
+print(text_slicing[0:5]) # 변수[a:b] : 문자열 중 a 이상 b 미만 번째 문자 출력
+print(text_slicing[:5]) # 변수[:a] : 문자열 처음부터 a 미만 번째 문자 출력
+print(text_slicing[5:]) # 변수[a:] : 문자열 a부터 마지막 문자까지 출력
+print(text_slicing[-1:]) # 맨 뒤 문자위치는 -1
+
