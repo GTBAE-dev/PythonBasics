b4e0bed4bbef1fd1bc124f9640015493d0e1396c
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 17:44:37 +0900

    1-4. 변수

--- Chapter1. 자료형과 변수.py
+++ Chapter1. 자료형과 변수.py
@@ -25,3 +25,14 @@ print(True)
 print(False) # 시작 문자가 대문자여야 Boolean 으로 인식
 print(not True) # not 은 반대로 출력
 
+''' 1-4. 변수 '''
+# 변수 : 값을 저장하는 공간
+animal = "소"
+print(type(animal)) #변수의 자료형 출력 
+age = 4
+print(type(age))
+is_adult = age >= 3
+print(type(is_adult))
+# +를 사용하여 합칠 시 str 자료형만 출력 가능, 다른 자료형은 str으로 바꿔주어야함 
+print(animal + "는 " + str(age) + "살이면 어른일까?" + str(is_adult)) # 다른 자료형으로 바꾸는 방법: str()
+print(animal, "는", age, "살이면 어른일까?", is_adult) # , 를 사용하여 합칠 시 모든 자료형 출력 가능
