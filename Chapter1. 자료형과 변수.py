129c8f00225a1ea5b82bf6a73bf6a964e64bb630
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 17:43:49 +0900

    1-2. 문자열 자료형

--- Chapter1. 자료형과 변수.py
+++ Chapter1. 자료형과 변수.py
@@ -9,3 +9,10 @@ print(10-10)
 print(10*10)
 print(10/10)
 
+''' 1-2. 문자열 자료형 '''
+# 문자열은 작은 따옴표 또는 큰 따옴표로 감싸줘야함
+print('Hello World')
+print("Hello World")
+# 문자열 곱셈 가능(이외의 사직연산은 불가)
+print("Hello World"*3)
+
