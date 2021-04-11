678af083246eb351859798069729b6e17b34de8e
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 18:20:36 +0900

    2-3. 숫자처리함수

--- Chapter2. 연산과 숫자처리함수.py
+++ Chapter2. 연산과 숫자처리함수.py
@@ -41,3 +41,22 @@ print((4>0) or (4>5)) # A or B ; A와 B 중 하나만 True면 True
 print((4>0) | (4>5)) # |(shift+\) = or
 print(5>4>6) # 이중비교
 
+''' 2-3. 숫자처리함수 '''
+print(abs(-4)) # abs(a) ; a의 절대값
+print(pow(4,3)) # pow(a,b) ; a^b
+print(max(4,3)) # max(a,b) ; a와 b 중 최대값
+print(min(4,3)) # min(a,b) ; a와 b 중 최소값
+print(round(4.51)) # round(a) ; a의 반올림 값
+# math library 활용
+from math import *
+print(floor(4.99)) # floor(a) ; a의 내림 값
+print(ceil(4.01)) # floor(a) ; a의 올림 값
+print(sqrt(16)) # sqrt(a) ; a의 제곱근
+# random library 활용
+from random import *
+print(random()) # 0.0 ~ 1.0 미만 임의 값 생성
+print(random()*10) # 0.0 ~ 10.0 미만 임의 값 생성
+print(int(random()*10)) # 0 ~ 10 미만 임의 값 생성(정수처리)
+print(int(random()*10+1)) # 1 ~ 10 이하 임의 값 생성
+print(randrange(1,10)) # randrange(a,b) ; a 이상 b 미만 임의 정수 값 생성
+print(randint(1,10)) # randint(a,b) ; a 이상 b 이하 임의 정수 값 생성
