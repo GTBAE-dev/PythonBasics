912d475ca084232cfc1128309a335d53fb52bce9
GTBAE-dev <gyutak93@gmail.com>
2021-04-11 18:02:40 +0900

    2-1. 사칙연산

---
+++ Chapter2. 연산과 숫자처리함수.py
@@ -0,0 +1,27 @@
+''' 2-1. 사칙연산 '''
+# 계산 결과를 출력
+print(4+4)
+print(4-4)
+print(4*4)
+print(4/4)
+print((4+4)*4) #우선 순위 연산 가능
+print(4**4) # a**b ; a의 b제곱
+print(5//4) # a//b ; a나누기 b의 몫
+print(5%4) # a%b ; a나누기 b의 나머지
+# 변수 연산
+number = 4
+print(number)
+number += 4 # number = number + 4 와 동일
+print(number)
+number -= 4 # number = number - 4 와 동일
+print(number)
+number /= 4 # number = number / 4 와 동일
+print(number)
+number *= 4 # number = number * 4 와 동일
+print(number)
+number **= 4 # number = number ** 4 와 동일
+print(number)
+number //= 4 # number = number // 4 와 동일
+print(number)
+number %= 4 # number = number % 4 와 동일
+print(number)
