93e6a3f0b6bd19dc5d09f5b3ddf53c153a47ae53
GTBAE-dev <gyutak93@gmail.com>
2021-04-14 20:40:53 +0900

    5-1. if 문

---
+++ Chapter5. 조건문과 반복문.py
@@ -0,0 +1,11 @@
+''' 5-1. if 문 '''
+# if문 : 조건 실행명령문
+a = "AA"
+b = "BB"
+if a == "CC" : # if 조건에 해당하는 실행문
+    print("a = AA")
+elif b == "CC" : # elif 조건에 해당하는 실행문(여러개 설정 가능)
+    print("b = BB")
+else : # 모두 해당되지 않을 때의 실행문
+    print("c = CC")
+
