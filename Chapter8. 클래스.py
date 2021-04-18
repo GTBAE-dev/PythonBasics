7f2131959edc5d0196dfa61fa70e88022684c9a6
GTBAE-dev <gyutak93@gmail.com>
2021-04-18 21:22:10 +0900

    8-2. 메소드

--- Chapter8. 클래스.py
+++ Chapter8. 클래스.py
@@ -7,3 +7,22 @@ class alpha_1 : # alphabet 이라는 클래스 생성
         print(self.lowfirst, self.num_1)
 AA = alpha_1("aA", 1) # AA는 alphabet 클래스의 instance
 BB = alpha_1("bB", 2)
+
+''' 8-2. 메소드 '''
+# 메소드 : 클래스 내의 함수
+class alpha_2 : 
+    def __init__(self, upfirst, num_2) : 
+        self.upfirst = upfirst
+        self.num_2 = num_2
+        print(self.upfirst, self.num_2)
+    def multiple(self, num_def) : # 함수 및 전달값 정의 방법
+        if num_def > 1 : 
+            for i in range(1, num_def + 1) : 
+                print(self.upfirst * i)
+        else : 
+            for i in range(0, self.num_2) : 
+                print(self.upfirst * self.num_2)
+                self.num_2 -= 1
+CC = alpha_2("Cc", 3)
+CC.multiple(4) # 변수.함수() : 클래스 내 함수 사용 방법
+CC.multiple(0)
